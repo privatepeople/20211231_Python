471180a627b34d6049a8b81ea8675ded666ff8b7
Lee Seung Hun <negrat@naver.com>
2021-12-31 11:38:11 +0900

    DB에서 내려준 일자 -> 양식을 갖춘 str으로 가공해서 출력

--- models/users.py
+++ models/users.py
@@ -16,16 +16,19 @@ class Users():
         self.height = info_dict['height']
         self.created_at = info_dict['created_at']
         self.friend_id = info_dict['friend_id']
-    
+
     # 사용자의 정보를 가공해서 간략하게 출력
-    # 이름 - 35세, 남성
+    # 이름 - 35세, 남성 (210805 가입)
     def get_simple_info(self):
-        print(f'{self.name} - {self.get_age()}세, {self.gender}')
+
+        format_date = self.created_at.strftime('%y%m%d')
+        
+        print(f'{self.name} - {self.get_age()}세, {self.gender} ({format_date} 가입)')
     
     # 사용자의 나이를 계산해주는 기능 추가
     def get_age(self):
         # 현재 일자의 연도를 얻어오면, 자동으로 현재 나이 계산 기능
         
-        now_year = datetime.datetime.now().year
+        now_year = datetime.datetime.utcnow().year # 세계 표준 시간 -> DB 기준 시간대 적용
         
         return now_year - self.birth_year + 1
