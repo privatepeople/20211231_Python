42e13d9123fb4b8ccff513cfb605eea4350d00f8
Lee Seung Hun <negrat@naver.com>
2021-12-31 10:15:55 +0900

    함수를 실행 처리
    - 코드 로딩 과정에서 바로 실행 : 함수부터 만들고 호출해야함
    
    - 실제 사용 중에 가져다 쓰는 함수 : 코드의 위아래에 관계없이 사용 가능

--- main.py
+++ main.py
@@ -12,4 +12,15 @@ def show_main_menu():
     if num == 0:
         pass
     elif num == 1:
-        pass
+        # 코드를 불러낼떄 진짜 실행되는게 아님
+        # 코드가 다 불러지고 => 메뉴도 다 나타나고 => 키보드로 1번을 골랐을때가 되어야 실행
+        # 함수 안의 다른 함수 호출 : 위아래 순서에 관계 없이 불러낼 수 있다.
+        get_user_list_from_db()
+
+# 1번 누르면 => DB에서 수강생 목록 조회를 요청하는 기능
+def get_user_list_from_db():
+    print('db_handler에게 사용자 목록을 요청')
+
+# python 명령어로 실행될떄 => 위에서부터 밑으로 한줄씩 순서대로 실행됨
+# 함수도 만들어 두고 나서 사용해야함
+show_main_menu()
